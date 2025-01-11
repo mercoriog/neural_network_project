c7bd759ed7b870c1ba5b56f87eada2b14e3c17ec
Antonio Todisco <antoniotodisco3801@gmail.com>
2025-01-11 19:20:26 +0100

    update Rprop

--- src/RProp.py
+++ src/RProp.py
@@ -1,10 +1,9 @@
 import tensorflow as tf
-import numpy as np
 
 class RProp(tf.keras.optimizers.Optimizer):
     def __init__(self, init_alpha=1e-3, scale_up=1.2, scale_down=0.5, min_alpha=1e-6, max_alpha=50., learning_rate=1e-3, **kwargs):
-        # Chiama il costruttore della classe base, passando il parametro 'learning_rate'
-        super(RProp, self).__init__(**kwargs)
+        # Chiama il costruttore della classe base
+        super(RProp, self).__init__(learning_rate=learning_rate, **kwargs)
 
         # Imposta i parametri dell'RProp
         self.init_alpha = init_alpha
@@ -13,32 +12,30 @@ class RProp(tf.keras.optimizers.Optimizer):
         self.min_alpha = min_alpha
         self.max_alpha = max_alpha
 
-        # Usa il parametro 'learning_rate' della classe base
-        self.learning_rate = learning_rate
+    def _create_slots(self, var_list):
+        # Crea gli slot per memorizzare i gradienti precedenti e i valori di alpha
+        for var in var_list:
+            self.add_slot(var, "old_grad", initializer="zeros")  # Slot per i gradienti precedenti
+            self.add_slot(var, "alpha", initializer=tf.constant_initializer(self.init_alpha))  # Slot per alpha
 
-    def get_updates(self, params, constraints, loss):
-        grads = self.get_gradients(loss, params)
-        alphas = [tf.Variable(np.ones_like(p) * self.init_alpha) for p in params]
-        old_grads = [tf.Variable(np.zeros_like(p)) for p in params]
-        self.weights = alphas + old_grads
-        self.updates = []
+    def update_step(self, grad, var, learning_rate=None):
+        # Ottieni i gradienti precedenti e i valori di alpha
+        old_grad = self.get_slot(var, "old_grad")
+        alpha = self.get_slot(var, "alpha")
 
-        for param, grad, old_grad, alpha in zip(params, grads, old_grads, alphas):
-            new_alpha = tf.where(
-                grad * old_grad > 0,
-                tf.minimum(alpha * self.scale_up, self.max_alpha),
-                tf.maximum(alpha * self.scale_down, self.min_alpha)
-            )
-            new_param = param - tf.sign(grad) * new_alpha
-            # Apply constraints
-            if param in constraints:
-                c = constraints[param]
-                new_param = c(new_param)
-            self.updates.append(param.assign(new_param))
-            self.updates.append(alpha.assign(new_alpha))
-            self.updates.append(old_grad.assign(grad))
+        # Aggiorna alpha in base al prodotto tra il gradiente corrente e quello precedente
+        new_alpha = tf.where(
+            grad * old_grad > 0,
+            tf.minimum(alpha * self.scale_up, self.max_alpha),
+            tf.maximum(alpha * self.scale_down, self.min_alpha)
+        )
 
-        return self.updates
+        # Aggiorna i pesi
+        var.assign_sub(tf.sign(grad) * new_alpha)
+
+        # Aggiorna i gradienti precedenti e i valori di alpha
+        old_grad.assign(grad)
+        alpha.assign(new_alpha)
 
     def get_config(self):
         config = {
@@ -47,7 +44,7 @@ class RProp(tf.keras.optimizers.Optimizer):
             'scale_down': self.scale_down,
             'min_alpha': self.min_alpha,
             'max_alpha': self.max_alpha,
-            'learning_rate': self.learning_rate  # Conserve 'learning_rate' nella configurazione
+            'learning_rate': self.learning_rate
         }
         base_config = super(RProp, self).get_config()
         return {**base_config, **config}
