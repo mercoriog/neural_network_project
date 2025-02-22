fixed model without softmax, add early stopping in trainer and test config hidden layer=1

src/modelCompiler.py
@@ -46,7 +46,7 @@ class NN(nn.Module):
                 x = self.activation_function(x)
         
         # Applica softmax all'ultimo layer
-        x = F.softmax(x, dim=1)
+        #x = F.softmax(x, dim=1)
         
         return x
     